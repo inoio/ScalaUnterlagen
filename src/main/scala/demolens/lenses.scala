9e7518e1cff2e51db10e1a68b305ba27ea0ebba2
Markus Klink <justjoheinz@gmail.com>
2014-01-21 14:24:37 +0100

    reorganized lenses

--- src/main/scala/demolens/lenses.scala
+++ src/main/scala/demolens/lenses.scala
@@ -6,35 +6,38 @@ import Scalaz._
 object lenses extends App {
 
   case class Person(name: String, address: List[Address])
+  object Person {
+    val name: Person @> String =
+      Lens.lensu[Person, String](
+        set = { (p, n) => p.copy(name = n) },
+        get = { _.name })
+
+    val address: Person @> List[Address] =
+      Lens.lensu[Person, List[Address]](
+        set = (p, a) => p.copy(address = a),
+        get = { _.address })
+
+    // partial converts total lenses into partial lenses
+    def address(nth: Int): Person @?> Address =
+      Person.address.partial andThen PLens.listNthPLens(nth)
+  }
+
   case class Address(street: String, nr: Int)
-
-  val pNameLens: Person @> String =
-    Lens.lensu[Person, String]({ (p, n) => p.copy(name = n) }, { _.name })
-
-  val personAddresses: Person @> List[Address] =
-    Lens.lensu[Person, List[Address]](
-      (p, a) => p.copy(address = a),
-      _.address)
-
-  val aStreetLens: Address @> String =
-    Lens.lensu[Address, String]({ (a, s) => a.copy(street = s) }, { _.street })
-
-  // partial converts total lenses into partial lenses
-  def personNthAddress(nth: Int) =
-    personAddresses.partial andThen PLens.listNthPLens(nth)
-
-  // ~ is a shortcut for partial
-  def personNthAddressStreet(nth: Int) =
-    personNthAddress(nth: Int) andThen ~aStreetLens
+  object Address {
+    val street: Address @> String =
+      Lens.lensu[Address, String](
+        set = { (a, s) => a.copy(street = s) },
+        get = { _.street })
+  }
 
   val person = Person("Klink", List(Address("Somestreet", 29), Address("AnotherStreet", 30)))
 
   // get the according lens, call the setter with the person and value 
-  val newP = (personNthAddress(1)).set(person, Address("BakerStreet", 221))
+  val newP = (Person.address(1)).set(person, Address("BakerStreet", 221))
 
   println(newP)
   // trying to access the 3rd address (which does not exist)
-  val invalid = (personNthAddress(3)).set(person, Address("BakerStreet", 221))
+  val invalid = (Person.address(3)).set(person, Address("BakerStreet", 221))
 
   println(invalid)
 }
