7fa0a2accdb8e672a618d4f977da50bfe22a5cd2
Markus Klink <justjoheinz@gmail.com>
2014-01-23 15:57:07 +0100

    example for cake pattern with mockito specs2 test

---
+++ src/main/scala/cakepattern/cakepattern.scala
@@ -0,0 +1,33 @@
+package cakepattern
+
+trait SensorDataProviderComponent {
+  self =>
+  val sensorDataProvider: SensorDataProvider
+  class SensorDataProvider {
+
+    def getData() = {
+      println("Enter data:")
+      readLine()
+    }
+  }
+}
+
+trait SensorDataClientComponent {
+  self: SensorDataProviderComponent =>
+  val sensorDataClient: SensorDataClient
+
+  class SensorDataClient {
+
+    def sensorDatenAnzeigen() = {
+      val data = sensorDataProvider.getData()
+      println(data)
+      data
+    }
+  }
+}
+
+object Cake extends SensorDataProviderComponent with SensorDataClientComponent  {
+  val sensorDataProvider = new SensorDataProvider()
+  val sensorDataClient = new SensorDataClient()
+}
+
---
+++ src/test/scala/cakepattern/CakePatternSpec.scala
@@ -0,0 +1,21 @@
+package cakepattern
+
+import org.specs2.mutable._
+import org.specs2.mock.Mockito
+class CakePatternSpec extends Specification with Mockito {
+  
+  object CakeTest extends SensorDataClientComponent with SensorDataProviderComponent {
+    val sensorDataClient = new SensorDataClient()
+    val sensorDataProvider = mock[SensorDataProvider]
+  }
+  
+  import CakeTest._
+  
+  "The caketest" should {
+    "deliver correct sensor results " in {
+      sensorDataProvider.getData() returns "3"
+      sensorDataClient.sensorDatenAnzeigen() must be equalTo("3")
+    }
+  }
+
+}
