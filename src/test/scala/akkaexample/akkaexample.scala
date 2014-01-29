5fcc04d2eb53b43a6c4935e50c31af86762f8aaa
Markus Klink <justjoheinz@gmail.com>
2014-01-29 17:32:47 +0100

    test include time duration

--- src/test/scala/akkaexample/akkaexample.scala
+++ src/test/scala/akkaexample/akkaexample.scala
@@ -5,6 +5,7 @@ import org.specs2.mutable._
 import org.specs2.time._
 import oose.akka.test._
 import akka.testkit._
+import scala.concurrent.duration._
 
 class AkkaExampleSpec extends Specification with NoTimeConversions {
 
@@ -12,27 +13,27 @@ class AkkaExampleSpec extends Specification with NoTimeConversions {
   "The monitor" should {
 
     "reply with up " in new AkkaSpecs2Scope {
-      val monitorActor = system.actorOf(Props[Monitor], "monitor")
+      val nodeActor = system.actorOf(Props[NetworkNode], "node")
+      val monitorActor = system.actorOf(Props(classOf[Monitor], nodeActor), "monitor")
 
       monitorActor ! IsUpRequest
-      expectMsg(Up)
+      expectMsg(5 seconds, Up)
     }
-    
+
     "repeat the test" in new AkkaSpecs2Scope {
       val nodeActor = system.actorOf(Props[NetworkNode], "node")
       val monitorActor = system.actorOf(Props(classOf[Monitor], nodeActor), "monitor")
 
       monitorActor ! IsUpRequest
-      expectMsg(Up)
+      expectMsg(5 seconds, Up)
     }
 
     "does the monitor actually ping the node" in new AkkaSpecs2Scope {
       val nodeActor = TestProbe()
       val monitorActor = system.actorOf(Props(classOf[Monitor], nodeActor.ref), "monitor")
 
-
       monitorActor ! IsUpRequest
-      nodeActor.expectMsg(Ping)
+      nodeActor.expectMsg(5 seconds, Ping)
     }
   }
 }
