9a73efdda02d9b9d4bffe67740819224980536cb
Markus Klink <justjoheinz@gmail.com>
2014-02-02 16:19:50 +0100

    added fizzbuzz kata

---
+++ src/main/scala/kata/fizzbuzz/fizzbuzz.scala
@@ -0,0 +1,17 @@
+package kata.fizzbuzz
+
+trait Fizzbuzz {
+  def fizzbuzz(i: Int) : String = ""
+}
+
+trait Exercise {
+  sef: Fizzbuzz =>
+    /**
+     * @param i an Integer >= 0
+     * @return fizz if 3 is a divisor of i
+     *         buzz if 5 is a divisor of i
+     *         fizzbuzz if 3 and 5 is a divisor of i
+     *         i otherwise
+     */         
+    override def fizzbuzz(i: Int) = ""
+}
